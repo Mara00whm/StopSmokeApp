3d393672cf32710427554d73dac732a4effe8bc9
Саляхетдинов Марат Русланович <marat.salyaxetdinov@aisa.ru>
2025-01-29 22:26:24 +0300

    Initial Commit

---
+++ StopSmoke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StopSmoke
+//
+//  Created by Марат Саляхетдинов on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
